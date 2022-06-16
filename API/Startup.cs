0dd554ef10dfb3c3ff5a6343e5ba758e2ce56b40
Sebastian Reza <jsrezam@gmail.com>
2022-06-15 23:51:05 -0500

    End of secion 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,7 +34,7 @@ namespace API
                 {
                     policy.AllowAnyHeader()
                     .AllowAnyMethod()
-                    .WithOrigins("https:localhost:4200");
+                    .WithOrigins("https://localhost:4200");
                 });
             });
         }
