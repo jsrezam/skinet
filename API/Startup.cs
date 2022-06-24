bccca498430d74be6b20e4c25607438f07b5d8d0
Sebastian Reza <jsrezam@gmail.com>
2022-06-24 17:23:32 -0500

    End of section 15

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -24,9 +25,15 @@ namespace API
         {
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
+
             services.AddDbContext<StoreContext>(builder =>
                 builder.UseSqlite(_config.GetConnectionString("DefaultConnection")
             ));
+
+            services.AddDbContext<AppIdentityDbContext>(builder =>
+                builder.UseSqlite(_config.GetConnectionString("IdentityConnection")
+            ));
+
             services.AddSingleton<IConnectionMultiplexer>(_ =>
             {
                 var configuration = ConfigurationOptions
@@ -35,6 +42,7 @@ namespace API
             });
 
             services.AddApplicationServices();
+            services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
             services.AddCors(options =>
             {
@@ -56,6 +64,7 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
             app.UseCors("CorsPolicy");
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
             app.UseEndpoints(endpoints => endpoints.MapControllers());
---
+++ Infrastructure/Identity/Migrations/20220624160029_RenameLastnNameColumnNameToLastNameFromAddressTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Identity.Migrations
+{
+    public partial class RenameLastnNameColumnNameToLastNameFromAddressTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastnName",
+                table: "Address",
+                newName: "LastName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Address",
+                newName: "LastnName");
+        }
+    }
+}
